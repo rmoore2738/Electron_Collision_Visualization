257eb8233b9c2d280335cff93d50f886118b5ade
Rebecca Moore <80188565+rmoore2738@users.noreply.github.com>
2025-03-27 12:36:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc, html
 from dash.dependencies import Input, Output, State
 import pandas as pd
 import numpy as np
